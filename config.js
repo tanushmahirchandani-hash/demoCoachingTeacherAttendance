4724fb0a52a03980beb54f4d4cf314d9fe23944e
tanushmahirchandani-hash <tanushmahirchandani@gmail.com>
2025-10-14 11:57:47 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js — edit these for branding or new apps
+const ORG_NAME = "Super Admin Institute"; 
+const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbxBCCm7-LIsqbM3zPSLsZRmn4F0Wp_9Tt7xAMK5bfq-XYtOvyVb1LisG9riCOKSH16W/exec"; 
---
+++ passwords.js
@@ -0,0 +1,14 @@
+// passwords.js — edit to change passwords
+const PAGE_PASSWORDS = {
+  "generator.html": "admin123",
+  "view.html": "viewer123",
+  "scan.html": {
+      "SNGR": "sngr123",
+      "PN": "pn123",
+      "JP": "jp123",
+      "VN": "vn123",
+      "GOPALPURA": "gopal123",
+      "JHOTWARA": "jhot123",
+      "RAJA-PARK": "raja123"
+  }
+};
